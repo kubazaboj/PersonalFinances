4e7670ad65c66f32dc4109dd7f2ff4ce751d6500
Jakub Záboj <zabojjak@student.cvut.cz>
2023-06-09 12:13:34 +0200

    Added tests for Investment class, also added selling shares handling

--- src/main/java/org/example/Investment.java
+++ src/main/java/org/example/Investment.java
@@ -6,18 +6,19 @@ import java.util.List;
 
 public class Investment {
     private String name;
-    private double quantity;
     private  List<Double> purchasePrices;
+    private List<Double> purchaseShares;
 
     private List<LocalDate> purchaseDates;
 
-    public Investment(String name, int quantity, double initialPurchasePrice, LocalDate initialPurchaseDate) {
+    public Investment(String name, double initialSharesBought, double initialPurchasePrice, LocalDate initialPurchaseDate) {
         this.name = name;
-        this.quantity = quantity;
         this.purchasePrices = new ArrayList<>();
         this.purchaseDates = new ArrayList<>();
+        this.purchaseShares = new ArrayList<>();
         this.purchasePrices.add(initialPurchasePrice);
         this.purchaseDates.add(initialPurchaseDate);
+        this.purchaseShares.add(initialSharesBought);
     }
 
 
@@ -31,9 +32,11 @@ public class Investment {
         this.name = name;
     }
 
-    public double getQuantity() {
-        return quantity;
+    public List<Double> getPurchaseShares() {
+        return purchaseShares;
     }
+
+    public double getTotalNumberOfShares() {return purchaseShares.stream().mapToDouble(Double::doubleValue).sum();}
 
     public List<Double> getPurchasePrices() {
         return purchasePrices;
@@ -46,11 +49,49 @@ public class Investment {
     public void addPurchase(double price, LocalDate date, double quantity) {
         purchasePrices.add(price);
         purchaseDates.add(date);
-        this.quantity += quantity;
+        purchaseShares.add(quantity);
+    }
+
+    public void addSale(double price, LocalDate date, double quantity){
+        purchasePrices.add(-1 * price);
+        purchaseDates.add(date);
+        purchaseShares.add(-1 * quantity);
+    }
+
+    public int getNumberofBuys(){
+        int numberBuys = 0;
+        for (double purchasePrice:purchasePrices) {
+            if (purchasePrice > 0){
+                numberBuys += 1;
+            }
+        }
+        return numberBuys;
+    }
+    public int getNumberofSells(){
+        int numberSells = 0;
+        for (double purchasePrice:purchasePrices) {
+            if (purchasePrice < 0){
+                numberSells += 1;
+            }
+        }
+        return numberSells;
     }
 
     public double AveragePurchasePrice(){
-        return (purchasePrices.stream().mapToDouble(Double::doubleValue).sum()) / quantity;
+        double sumAllPrices = 0.0;
+        if (purchasePrices.size() != purchaseShares.size()){
+            System.err.println("Cannot calculate average purchase price, not every purchase has price or number of shares added");
+            return sumAllPrices;
+        }
+        for (int i = 0; i < purchasePrices.size(); i++) {
+            if (purchasePrices.get(i) < 0 || purchaseShares.get(i) < 0){
+                sumAllPrices -= purchasePrices.get(i) * purchaseShares.get(i);
+
+            }else {
+                sumAllPrices += purchasePrices.get(i) * purchaseShares.get(i);
+            }
+        }
+        return sumAllPrices / getTotalNumberOfShares();
     }
 
     public LocalDate getCheapestBuyDate(){
@@ -69,7 +110,7 @@ public class Investment {
 
         for (int i = 0; i < purchasePrices.size(); i++) {
             double current = purchasePrices.get(i);
-            if (current < minPrice) {
+            if (current < minPrice && current > 0) {
                 minPrice = current;
                 minPriceIndex = i;
             }
@@ -93,7 +134,55 @@ public class Investment {
 
         for (int i = 0; i < purchasePrices.size(); i++) {
             double current = purchasePrices.get(i);
-            if (current > maxPrice) {
+            if (current > maxPrice && current > 0) {
+                maxPrice = current;
+                maxPriceIndex = i;
+            }
+        }
+        return maxPriceIndex;
+    }
+
+    public LocalDate getCheapestSellDate(){
+        int cheapestBuyDateIndex = getCheapestSellIndex();
+        return purchaseDates.get(cheapestBuyDateIndex);
+    }
+
+    public double getCheapestSellAmount(){
+        int cheapestBuyDateIndex = getCheapestSellIndex();
+        return purchasePrices.get(cheapestBuyDateIndex) * (-1);
+    }
+
+    private int getCheapestSellIndex(){
+        double minPrice = Double.MAX_VALUE * (-1);
+        int minPriceIndex = -1;
+
+        for (int i = 0; i < purchasePrices.size(); i++) {
+            double current = purchasePrices.get(i);
+            if (current > minPrice && current < 0) {
+                minPrice = current;
+                minPriceIndex = i;
+            }
+        }
+        return minPriceIndex;
+    }
+
+    public LocalDate getMostExpensiveSellDate(){
+        int mostExpensiveBuyDateIndex = getMostExpensiveSellIndex();
+        return purchaseDates.get(mostExpensiveBuyDateIndex);
+    }
+
+    public double getMostExpensiveSellAmount(){
+        int mostExpensiveBuyDateIndex = getMostExpensiveSellIndex();
+        return purchasePrices.get(mostExpensiveBuyDateIndex) * (-1);
+    }
+
+    private int getMostExpensiveSellIndex(){
+        double maxPrice = Double.MAX_VALUE;
+        int maxPriceIndex = -1;
+
+        for (int i = 0; i < purchasePrices.size(); i++) {
+            double current = purchasePrices.get(i);
+            if (current < maxPrice && current < 0) {
                 maxPrice = current;
                 maxPriceIndex = i;
             }
@@ -103,7 +192,7 @@ public class Investment {
 
     public double getInvestmentLossGain(double actualPrice){
         double investmentPriceDelta = actualPrice - AveragePurchasePrice();
-        return quantity * investmentPriceDelta;
+        return getTotalNumberOfShares() * investmentPriceDelta;
     }
 
 }
---
+++ src/test/java/org/example/InvestmentTest.java
@@ -0,0 +1,200 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InvestmentTest {
+
+    Investment investment;
+    double initialPurchasePrice = 10.0;
+    double initialSharesBought = 100;
+    @BeforeEach
+    public void beforeEach(){
+        investment = new Investment("Test Investment", initialSharesBought, initialPurchasePrice, LocalDate.now());
+    }
+
+    @Test
+    public void testAddPurchase() {
+
+        investment.addPurchase(15.0, LocalDate.now().plusDays(1), 50);
+
+        assertEquals(150, investment.getTotalNumberOfShares());
+        assertEquals(2, investment.getPurchasePrices().size());
+        assertEquals(2, investment.getPurchaseDates().size());
+    }
+
+    @Test
+    public void testAddSale() {
+        investment.addPurchase(20.0, LocalDate.now(), 3);
+
+        assertEquals(2, investment.getNumberofBuys());
+
+        investment.addSale(15.0, LocalDate.now(), 1);
+
+        assertEquals(2, investment.getNumberofBuys());
+        assertEquals(1, investment.getNumberofSells());
+    }
+
+    @Test
+    public void testGetNumBuys() {
+        investment.addPurchase(10.0, LocalDate.now().plusDays(1), 4);
+        investment.addPurchase(0.0, LocalDate.now().plusDays(2), 0.0);
+
+        assertEquals(2, investment.getNumberofBuys());
+    }
+
+    @Test
+    public void testGetNumSells() {
+        investment.addSale(5, LocalDate.now(), 15);
+        investment.addSale(8, LocalDate.now(), 15);
+        assertEquals(2, investment.getNumberofSells());
+    }
+    @Test
+    public void testAveragePurchasePriceWithoutSale() {
+        investment.addPurchase(15.0, LocalDate.now().plusDays(1), 50);
+        assertEquals(11.6667, investment.AveragePurchasePrice(), 0.0001);
+    }
+
+    @Test
+    public void testAveragePurchasePriceWithSale() {
+        investment.addPurchase(20.0, LocalDate.now().plusDays(1), 100);
+        investment.addSale(20.0, LocalDate.now().plusDays(1), 50);
+        assertEquals(13.3334, investment.AveragePurchasePrice(), 0.001);
+    }
+
+    @Test
+    public void testGetCheapestBuyDate() {
+        investment.addPurchase(8.0, LocalDate.now().plusDays(1), 50);
+        investment.addPurchase(8.01, LocalDate.now().plusDays(2), 0.7);
+        investment.addSale(4, LocalDate.now().plusDays(27), 1);
+        assertEquals(LocalDate.now().plusDays(1), investment.getCheapestBuyDate());
+    }
+
+    @Test
+    public void testGetCheapestBuyAmount() {
+        investment.addPurchase(8.0, LocalDate.now().plusDays(1), 50);
+        investment.addSale(4, LocalDate.now().plusDays(27), 1);
+        assertEquals(8.0, investment.getCheapestBuyAmount());
+    }
+
+    @Test
+    public void testGetMostExpensiveBuyDate() {
+        investment.addPurchase(9.5, LocalDate.now().plusDays(1), 50);
+        investment.addPurchase(10.01, LocalDate.now().plusDays(2), 0.7);
+        investment.addSale(250.05, LocalDate.now().plusDays(27), 1);
+
+        assertEquals(LocalDate.now().plusDays(2), investment.getMostExpensiveBuyDate());
+    }
+
+    @Test
+    public void testGetMostExpensiveBuyAmount() {
+        investment.addPurchase(15.0, LocalDate.now().plusDays(1), 50);
+        investment.addSale(250.05, LocalDate.now().plusDays(27), 1);
+        assertEquals(15.0, investment.getMostExpensiveBuyAmount());
+    }
+
+    @Test
+    public void testGetCheapestSellDate() {
+        investment.addPurchase(8.0, LocalDate.now().plusDays(1), 50);
+        investment.addPurchase(8.01, LocalDate.now().plusDays(2), 0.7);
+        investment.addSale(250.05, LocalDate.now().plusDays(27), 1);
+        investment.addSale(125.33, LocalDate.now().plusDays(29), 1);
+        assertEquals(LocalDate.now().plusDays(29), investment.getCheapestSellDate());
+    }
+
+    @Test
+    public void testGetCheapestSellAmount() {
+        investment.addPurchase(8.0, LocalDate.now().plusDays(1), 50);
+        investment.addSale(250.05, LocalDate.now().plusDays(27), 1);
+        investment.addSale(125.33, LocalDate.now().plusDays(29), 1);
+        assertEquals(125.33, investment.getCheapestSellAmount());
+    }
+
+    @Test
+    public void testGetMostExpensiveSellDate() {
+        investment.addPurchase(9.5, LocalDate.now().plusDays(1), 50);
+        investment.addPurchase(10.01, LocalDate.now().plusDays(2), 0.7);
+        investment.addSale(250.05, LocalDate.now().plusDays(27), 1);
+        investment.addSale(125.33, LocalDate.now().plusDays(29), 1);
+
+        assertEquals(LocalDate.now().plusDays(27), investment.getMostExpensiveSellDate());
+    }
+
+    @Test
+    public void testGetMostExpensiveSellAmount() {
+        investment.addPurchase(15.0, LocalDate.now().plusDays(1), 50);
+        investment.addSale(250.05, LocalDate.now().plusDays(27), 1);
+        investment.addSale(125.33, LocalDate.now().plusDays(29), 1);
+        assertEquals(250.05, investment.getMostExpensiveSellAmount());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "12.5",
+            "10.0",
+            "5,"
+    })
+    public void testGetInvestmentLossGain(double actualPrice) {
+        investment.addPurchase(15.0, LocalDate.now().plusDays(1), 50);
+        double[] allBoughtQuantities = {initialSharesBought, 50};
+        double weightedAverage = calculateWeightedAverage(new double[]{initialPurchasePrice, 15.0},
+                allBoughtQuantities);
+        double SumSharesBought = Arrays.stream(allBoughtQuantities).sum();
+
+        assertEquals((actualPrice - weightedAverage) * SumSharesBought, investment.getInvestmentLossGain(actualPrice), 0.001);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10.0, 20.0, 30.0, 40.0, 50.0",
+            "15.7, 12.8, 17.2, 13.1, 19.4",
+            "0.1, 2.4, 18,88, 25.1, 6.9"
+    })
+    public void testInvestmentMethods(double purchasePrice1, double purchasePrice2, double purchasePrice3,
+                                      double purchasePrice4, double purchasePrice5) {
+        double[] purchasePrices = {purchasePrice1, purchasePrice2, purchasePrice3,
+        purchasePrice4, purchasePrice5, initialPurchasePrice};
+
+        investment.addPurchase(purchasePrices[0], LocalDate.now().plusDays(1), 100);
+        investment.addPurchase(purchasePrices[1], LocalDate.now().plusDays(2), 50);
+        investment.addPurchase(purchasePrices[2], LocalDate.now().plusDays(3), 30);
+        investment.addPurchase(purchasePrices[3], LocalDate.now().plusDays(4), 20);
+        investment.addPurchase(purchasePrices[4], LocalDate.now().plusDays(5), 10);
+
+        double weightedAverage = calculateWeightedAverage(purchasePrices,
+                new double[]{100, 50, 30, 20, 10, initialSharesBought});
+
+        assertEquals(310, investment.getTotalNumberOfShares());
+        assertEquals(Arrays.stream(purchasePrices).min().orElse(0.0), investment.getCheapestBuyAmount());
+        assertEquals(Arrays.stream(purchasePrices).max().orElse(Double.MAX_VALUE), investment.getMostExpensiveBuyAmount());
+        assertEquals(weightedAverage, investment.AveragePurchasePrice(), 0.001);
+    }
+
+
+    private double calculateWeightedAverage(double[] values, double[] weights) {
+        if (values.length != weights.length) {
+            throw new IllegalArgumentException("Values and weights arrays must have the same length.");
+        }
+
+        double sum = 0.0;
+        double totalWeight = 0.0;
+
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i] * weights[i];
+            totalWeight += weights[i];
+        }
+
+        if (totalWeight == 0.0) {
+            throw new IllegalArgumentException("Total weight must be greater than zero.");
+        }
+
+        return sum / totalWeight;
+    }
+}
